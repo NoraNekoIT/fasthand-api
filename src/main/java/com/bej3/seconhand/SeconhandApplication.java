42bf2c1b1e5e30479b246e873562fab8f983b47c
NoraNekoIT <bunsmart18@gmail.com>
2022-06-21 10:28:05 +0700

    add config swagger

--- src/main/java/com/bej3/seconhand/SeconhandApplication.java
+++ src/main/java/com/bej3/seconhand/SeconhandApplication.java
@@ -1,17 +1,34 @@
 package com.bej3.seconhand;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com/bej3/seconhand/entities"})
+@OpenAPIDefinition(
+        info = @Info(title = "API Fasthand",
+                description = "STAGING VERSION -> API FASTHAND API BY BEJ-3 KELOMPOK 2",
+                version = "v1.0.0",
+                contact = @Contact(
+                        name = "",
+                        url = "",
+                        email = ""
+
+                ),
+                license = @License(
+                        name = "",
+                        url = ""
+                ),
+                termsOfService =""
+        )
+)
 public class SeconhandApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SeconhandApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SeconhandApplication.class, args);
+    }
 }
